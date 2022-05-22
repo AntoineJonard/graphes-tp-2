164dc1211e2f15ea8c141c84b98bed882c6c1f3f
Clemoix <clementd80@hotmail.fr>
2022-05-22 14:28:31 +0200

    feat('main'): Ajout des appel des fonction pour le test

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import graphe.Graphe;
+import cplex.*;
 
 public class Main {
 
@@ -6,6 +7,26 @@ public class Main {
 		// TODO Auto-generated method stub
 		Graphe g = new Graphe("reseau_10_10_1.txt");
 		System.out.println(g);
+		
+
+		Exemple1.solveMe();
+		
+		System.out.println("-----------------------------------------------");
+		System.out.println("===============================================");
+		System.out.println("-----------------------------------------------");
+		
+		Exemple2.solveMe();
+		
+		System.out.println("-----------------------------------------------");
+		System.out.println("===============================================");
+		System.out.println("-----------------------------------------------");
+		
+		VoyageurCommerce.solveMe(10);
+		
+		System.out.println("FIN");
+		
+		//PlusCourtChemin.solveMe(g);
+		
 	}
 
 }

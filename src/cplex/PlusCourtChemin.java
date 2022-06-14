ef3a9f0e65dcc929607e3b44ad333d9870154dd4
Clemoix <clementd80@hotmail.fr>
2022-06-15 00:16:41 +0200

    fix('PlusCourtChemin'): Fix du plus court Chemin où le problème était que les X et Y était inversé via des précédante modification

--- src/cplex/PlusCourtChemin.java
+++ src/cplex/PlusCourtChemin.java
@@ -21,30 +21,30 @@ public class PlusCourtChemin {
 		double[][] c = new double[n][n];
 		for (int i=0; i<n; i++) {
 			for (int j=0; j<n; j++) {
-				int xPosI = i/g.nbColonne();
-				int yPosI = i%g.nbColonne();
-				int xPosJ = j/g.nbColonne();
-				int yPosJ = j%g.nbColonne();
+				int xPosI = i%g.nbColonne();
+				int yPosI = i/g.nbColonne();
+				int xPosJ = j%g.nbColonne();
+				int yPosJ = j/g.nbColonne();
 				
 				//System.out.print("("+xPosI +"," + yPosI+") // ("+xPosJ +"," + yPosJ+")");
 				
 				c[i][j] = g.getSommet(xPosI,yPosI).getFlightDistTo(g.getSommet(xPosJ,yPosJ));
 				
-				System.out.print("\t"+c[i][j]);
+				//System.out.print("\t"+c[i][j]);
 			}
-			System.out.print("\n");
+			//System.out.print("\n");
 		}
 		
 		//d pour : l'ensemble des arêtes, entre le point i (x,y que l'on calcul) et j (x,y que l'on calcul)
 		double[][] d = new double[n][n];
 		for (int i=0; i<n; i++) {
 			for (int j=0; j<n; j++) {
-				int xPosI = i/g.nbColonne();
-				int yPosI = i%g.nbColonne();
-				int xPosJ = j/g.nbColonne();
-				int yPosJ = j%g.nbColonne();
+				int xPosI = i%g.nbColonne();
+				int yPosI = i/g.nbColonne();
+				int xPosJ = j%g.nbColonne();
+				int yPosJ = j/g.nbColonne();
 				
-				System.out.print("("+xPosI +"," + yPosI+") // ("+xPosJ +"," + yPosJ+")");
+				//System.out.print("("+xPosI +"," + yPosI+") // ("+xPosJ +"," + yPosJ+")");
 				
 				if((xPosJ<=xPosI+1 && xPosJ>=xPosI-1) && (yPosJ<=yPosI+1 && yPosJ>=yPosI-1)) {
 					if( (xPosI == xPosJ && yPosI == yPosJ) ) {
@@ -65,9 +65,9 @@ public class PlusCourtChemin {
 				}else {
 					d[i][j] = 0;
 				}
-				System.out.print("\t"+ d[i][j]);
+				//System.out.print("\t"+ d[i][j]);
 			}
-			System.out.print("\n");
+			//System.out.print("\n");
 		}
 		
 
@@ -80,7 +80,6 @@ public class PlusCourtChemin {
 			for (int i=0; i<n; i++) {
 				x[i] = cplex.boolVarArray(n);
 			}
-			IloNumVar[] u = cplex.numVarArray(n, 0, Double.MAX_VALUE);
 			
 			
 			//Objective
@@ -120,7 +119,7 @@ public class PlusCourtChemin {
 			cplex.addEq(expr, 1.0);
 			*/
 			
-			System.out.println(g);
+			//System.out.println(g);
 			
 			for (int i=0; i<n; i++) {
 				IloLinearNumExpr expr = cplex.linearNumExpr();
@@ -156,6 +155,7 @@ public class PlusCourtChemin {
         		System.out.println("Distance Minimal = " + cplex.getObjValue() + " ");
         		System.out.println();
         		
+        		/*//AFFICHAGE DU RESULTAT DE LA VARIABLE X TROUVER SOUS CPLEX CORRESPONDANT SI ON SE DEPLACE DE LA VILLE I VERS J
         		for(int i = 0; i<x.length; i++) {
         		    for(int j = 0 ; j < x[i].length; j ++) {
         		        if(i != j) {
@@ -165,7 +165,27 @@ public class PlusCourtChemin {
         		        } 
         		    }
         		    System.out.print("\n");
+        		}*/
+        		
+        		int arriver = 0;
+        		int point = g.getStart().getX() * g.getStart().getY();
+        		while(arriver == 0) {
+        			if(g.getSommet(point % g.nbColonne(), point / g.nbColonne()).getType().equals(Type.END)) {
+        				arriver = 1;
+        			}else {
+        				System.out.print("(" + point % g.nbColonne() + "," + point / g.nbColonne() + ") -> ");
+        				for(int i=0; i<x[point].length; i++) {
+        					if(point != i ) {
+        						if( cplex.getValue(x[point][i]) == 1.0 ) {
+            						point = i;
+            						break;
+            					}
+        					}
+        				}
+        				System.out.print("(" + point % g.nbColonne() + "," + point / g.nbColonne() + ")\n");
+        			}
         		}
+        				
 			}
 			else {
 				System.out.println("Model not solved");
